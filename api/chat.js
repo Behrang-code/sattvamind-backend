6480fb2526efb22a17fb6418942a9a1ee8d4bde7
Behrang-code <behrangyoga@gmail.com>
2025-04-15 19:58:38 +0200

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -10,7 +10,6 @@ export default async function handler(req, res) {
     headers: {
       "Content-Type": "application/json",
       "Authorization": "Bearer " + process.env.OPENAI_API_KEY
-
     },
     body: JSON.stringify({
       model: "gpt-3.5-turbo",
